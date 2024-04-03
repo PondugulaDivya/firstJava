ecd90d47104070930e690a65721b07a12bffd00e
PondugulaDivya <159253000+PondugulaDivya@users.noreply.github.com>
2024-04-03 11:10:10 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -1,6 +1,6 @@
 import java.io.*;
  
-class GFG {
+class Add {
     public static int sum(int num1, int num2)
     {
         return num1+num2;
@@ -8,7 +8,7 @@ class GFG {
      
     public static void main(String[] args)
     {
-        GFG ob = new GFG();
+        Add ob = new Add();
         int res = ob.sum(28, 49);
         System.out.println(res);
     }
