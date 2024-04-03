35e117c4ec13c0c93a052a28af2a4da960c17af2
PondugulaDivya <159253000+PondugulaDivya@users.noreply.github.com>
2024-04-03 14:38:59 +0530

    Update Sub.java

--- Sub.java
+++ Sub.java
@@ -3,13 +3,13 @@ import java.io.*;
 class Sub {
     public static int sum(int num1, int num2)
     {
-        return num1-num2;
+        return num1+num2;
     }
      
     public static void main(String[] args)
     {
         Sub ob = new Sub();
-        int res = ob.sum(50, 49);
+        int res = ob.sum(28, 49);
         System.out.println(res);
     }
 }
